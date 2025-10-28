4daff159c257cc1af17f019a1a11adde2d16fece
armita-d <armidarband@gmail.com>
2025-10-27 21:03:18 -0700

    code completed

--- Two_Sum.py
+++ Two_Sum.py
@@ -16,21 +16,3 @@ class Solution(object):
             for j in range(i, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i,j]
-
-
-'''
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        if len(nums) < 2:
-            return []
-        for i in range (len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
-        
-'''
